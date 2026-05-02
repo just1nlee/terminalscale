62e82147b147f537568863d87a79b712a7a1e48e
Justin Lee <leejustincs@gmail.com>
2026-05-02 12:42:36 -0700

    feat: setup PTY
    Signed-off-by: Justin Lee <leejustincs@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/just1nlee/terminalscale
 
 go 1.24.4
+
+require github.com/creack/pty v1.1.24
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"os"
+	"os/exec"
+
+	"github.com/creack/pty"
+)
 
 func main() {
-	fmt.Print("Hello, world!")
+	cmd := exec.Command("bash")
+
+	ptmx, err := pty.Start(cmd)
+	if err != nil {
+		panic(err)
+	}
+	defer ptmx.Close()
+
+	go io.Copy(os.Stdout, ptmx)
+
+	io.Copy(ptmx, os.Stdin)
 }
