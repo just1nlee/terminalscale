7b3e653bbc66629732af41c437b216eca3f93ebe
Justin Lee <leejustincs@gmail.com>
2026-05-02 12:46:33 -0700

    style: add initial comments
    Signed-off-by: Justin Lee <leejustincs@gmail.com>

--- main.go
+++ main.go
@@ -11,13 +11,15 @@ import (
 func main() {
 	cmd := exec.Command("bash")
 
+	// PTY handler
 	ptmx, err := pty.Start(cmd)
 	if err != nil {
 		panic(err)
 	}
 	defer ptmx.Close()
 
+	// Output
 	go io.Copy(os.Stdout, ptmx)
-
+	// Input
 	io.Copy(ptmx, os.Stdin)
 }
