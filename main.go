329b064f3ad7211cc7117fc0d377bcafc4be2b67
Justin Lee <leejustincs@gmail.com>
2026-05-02 13:10:08 -0700

    refactor: modularize code into entry point and pty.go
    Signed-off-by: Justin Lee <leejustincs@gmail.com>

--- main.go
+++ main.go
@@ -3,47 +3,25 @@ package main
 import (
 	"io"
 	"os"
-	"os/exec"
-	"os/signal"
-	"syscall"
-
-	"github.com/creack/pty"
-	"golang.org/x/term"
 )
 
 func main() {
-	cmd := exec.Command("bash")
-
 	// PTY pair handler
-	ptmx, err := pty.Start(cmd)
+	ptmx, err := NewPTY()
+	if err != nil {
+		panic(err)
+	}
+
+	err = ptmx.SaveState()
 	if err != nil {
 		panic(err)
 	}
 	defer ptmx.Close()
 
-	// Switch to raw mode when running programs
-	oldState, err := term.MakeRaw(int(os.Stdin.Fd())) // Saves cooked mode state
-	if err != nil {
-		panic(err)
-	}
-	// Switch back to cooked mode after program ends
-	defer term.Restore(int(os.Stdin.Fd()), oldState) // Restores cooked mode state
-
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, syscall.SIGWINCH)
-	go func() {
-		for range ch {
-			size, err := pty.GetsizeFull(os.Stdin)
-			if err != nil {
-				return
-			}
-			pty.Setsize(ptmx, size)
-		}
-	}()
-	ch <- syscall.SIGWINCH
+	ptmx.HandleResize()
 
 	// PTY Output
-	go io.Copy(os.Stdout, ptmx)
+	go io.Copy(os.Stdout, ptmx.Master)
 	// PTY Input
-	io.Copy(ptmx, os.Stdin)
+	io.Copy(ptmx.Master, os.Stdin)
 }
---
+++ pty.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+
+	"github.com/creack/pty"
+	"golang.org/x/term"
+)
+
+type PTY struct {
+	Master   *os.File
+	oldState *term.State
+}
+
+func NewPTY() (*PTY, error) {
+	// SHELL to run
+	cmd := exec.Command("bash")
+
+	master, err := pty.Start(cmd)
+	if err != nil {
+		return nil, err
+	}
+
+	return &PTY{Master: master}, nil
+}
+
+func (p *PTY) SaveState() error {
+	// Switch to raw mode when running programs
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd())) // Saves cooked mode state
+	if err != nil {
+		return err
+	}
+	p.oldState = oldState
+	return nil
+}
+
+func (p *PTY) RestoreState() {
+	if p.oldState != nil {
+		// Switch back to cooked mode after program ends
+		term.Restore(int(os.Stdin.Fd()), p.oldState) // Restores cooked mode state
+	}
+}
+
+func (p *PTY) HandleResize() {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGWINCH)
+	go func() {
+		for range ch {
+			size, err := pty.GetsizeFull(os.Stdin)
+			if err != nil {
+				return
+			}
+			pty.Setsize(p.Master, size)
+		}
+	}()
+	ch <- syscall.SIGWINCH
+}
+
+func (p *PTY) Close() {
+	p.RestoreState()
+	p.Master.Close()
+}
